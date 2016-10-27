1f29fd28bc9034f0f796bc26566d2bb4efb601b5
Aidan Gadberry <agadberr@gmail.com>
2016-10-26 21:27:10 -0700

    new iOS interface

---
+++ Accent-iOS/Accent/Accent/LandingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LandingViewController.swift
+//  Accent
+//
+//  Created by Aidan Gadberry on 10/26/16.
+//  Copyright © 2016 agadberr. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet var loginView: UIView!
+    @IBOutlet var loginButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
