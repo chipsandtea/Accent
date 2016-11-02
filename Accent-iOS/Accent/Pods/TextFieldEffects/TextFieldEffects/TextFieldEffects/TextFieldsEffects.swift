57e7d178b5bf7afd48638ab1a1c5644812055ae6
Aidan Gadberry <agadberr@gmail.com>
2016-11-02 16:20:04 -0700

    Fixed Podfile

--- Accent-iOS/Accent/Pods/TextFieldEffects/TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
+++ Accent-iOS/Accent/Pods/TextFieldEffects/TextFieldEffects/TextFieldEffects/TextFieldsEffects.swift
@@ -87,7 +87,7 @@ open class TextFieldEffects : UITextField {
     
     override open var text: String? {
         didSet {
-            if let text = text , text.isNotEmpty {
+            if let text = text, text.isNotEmpty {
                 animateViewsForTextEntry()
             } else {
                 animateViewsForTextDisplay()
