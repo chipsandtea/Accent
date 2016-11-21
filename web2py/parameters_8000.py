9be624a2be30ae9ad3aa977d7bcb3956aee69453
leslie <huishili95@gmail.com>
2016-11-21 19:01:13 +0000

    update README

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b7b80949fa40ba7c$7b164546424b7769506d0533d43345bda71adfcc"
+password="pbkdf2(1000,20,sha512)$9eee6c81046dc78f$a556f206c09764ec9cbdaaeaa2cf5ab24700cef1"
