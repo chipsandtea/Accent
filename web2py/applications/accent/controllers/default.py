1b303860f62d4a4d10cfe073d405b647db4c8b22
Jeremiah <jeremiahjliou@gmail.com>
2016-11-21 01:00:55 +0000

    deleted errors

--- web2py/applications/accent/controllers/default.py
+++ web2py/applications/accent/controllers/default.py
@@ -27,41 +27,55 @@ def index():
 def api():
     response.view = 'generic.json'
 
-    def GET(tablename, id):
-        if not tablename == 'acc':
-            raise HTTP(400)
-        return dict(acc = db.acc(id))
-
-    #temp returning status success until we have validation in place
+    def GET(*args, **vars):
+	status = None
+	error = None
+	#pattern matching for login
+	patterns = [
+	   "/login/{acc.email}/{acc.password}"
+	]
+	#maps request args from URL to a db query    	
+	parser = db.parse_as_rest(patterns, args, vars)
+	#successfully parsed the request
+        if parser.status == 200:
+	    #returns rows from db query
+	    resp = parser.response
+	    #no rows returned, email and pw did not match
+	    if len(resp) == 0:
+	        status = 'failure'
+		error = 'Email and password did not match'
+		return response.json(dict(status = status, error = error))	
+	    #return a response with the queried content
+	    else:
+		status = 'success'
+		row = resp[0]	
+	    return response.json(dict(status = status, content = row))
+	else:
+	    raise HTTP(400) 
+    
     def POST(tablename, **fields):
-        status = None
+	#========creating account================
         if tablename == 'acc':
-            #TODO: check if acc exists? look up validate_and_insert return value
+            #check if acc exists? look up validate_and_insert return value
 	    existing = db(db.acc.email == request.post_vars.email).select().first()
             if existing is None:
-       	        logger.info('email doesnt exist, success response')
-                status = 'success'
-		resp = dict(status = status, acc = db.acc.validate_and_insert(**fields))
+		resp = dict(status = 'success',acc = db.acc.validate_and_insert(**fields))
 	    else: 
-                logger.info('email exists')
             	resp = dict(status = 'failure', error = 'email already exists')
-		#logger.info('acc email field: ' + request.post_vars.email)
 
-            #resp = dict(status = status,acc = db.acc.validate_and_insert(**fields))
             return response.json(resp)
+
+	#=========test=========================
         elif tablename == 'test':
-	    status = 'success'
-	    resp = dict(status = status, test = demoCall())
-	    return response.json(resp)
+	    return response.json(dict(status = 'success', test = demoCall()))
+	
+	elif tablename == 'correct':
+	    return response.json(dict(status = 'success'))
+
+	#========invalid request===============
 	else:
-	    raise HTTP(400);
-      #  if table_name == 'person':
-       #     return dict(db.person.validate_and_insert(**vars))
-       # elif table_name == 'pet':
-       #     return dict(db.pet.validate_and_insert(**vars))
-       # else:
-       #     raise HTTP(400)  
-    #locals() returns a dictionary including all the variables in the local function         
+	    raise HTTP(400)
+   	#locals() returns a dict including all vars in the local func 
     return locals()      
 
 def user():
