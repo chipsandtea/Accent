179e51b929dedf905f8b4f9880dd550252ab8fd9
Christopher Hsiao <hsiao.christopher@gmail.com>
2016-11-23 21:51:26 -0800

    ginger returns fixed string

--- ginger.py
+++ ginger.py
@@ -4,6 +4,9 @@
 """Simple grammar checker
 
 This grammar checker will fix grammar mistakes using Ginger.
+
+Code placeholder for NLP functionality: https://github.com/zoncoen/python-ginger
+Credit to zoncoen
 """
 
 import sys
@@ -12,52 +15,6 @@ import urllib.request
 from urllib.error import HTTPError
 from urllib.error import URLError
 import json
-
-
-class ColoredText:
-    """Colored text class"""
-    colors = ['black', 'red', 'green', 'orange', 'blue', 'magenta', 'cyan', 'white']
-    color_dict = {}
-    for i, c in enumerate(colors):
-        color_dict[c] = (i + 30, i + 40)
-
-    @classmethod
-    def colorize(cls, text, color=None, bgcolor=None):
-        """Colorize text
-        @param cls Class
-        @param text Text
-        @param color Text color
-        @param bgcolor Background color
-        """
-        c = None
-        bg = None
-        gap = 0
-        if color is not None:
-            try:
-                c = cls.color_dict[color][0]
-            except KeyError:
-                print("Invalid text color:", color)
-                return(text, gap)
-
-        if bgcolor is not None:
-            try:
-                bg = cls.color_dict[bgcolor][1]
-            except KeyError:
-                print("Invalid background color:", bgcolor)
-                return(text, gap)
-
-        s_open, s_close = '', ''
-        if c is not None:
-            s_open = '\033[%dm' % c
-            gap = len(s_open)
-        if bg is not None:
-            s_open += '\033[%dm' % bg
-            gap = len(s_open)
-        if not c is None or bg is None:
-            s_close = '\033[0m'
-            gap += len(s_close)
-        return('%s%s%s' % (s_open, text, s_close), gap)
-
 
 def get_ginger_url(text):
     """Get URL for checking grammar using Ginger.
@@ -101,7 +58,7 @@ def get_ginger_result(text):
     except ValueError:
         print("Value Error: Invalid server response.")
         quit()
-
+    #print(result)
     return(result)
 
 
@@ -120,25 +77,18 @@ def main():
         quit()
 
     # Incorrect grammar
-    color_gap, fixed_gap = 0, 0
+    gap, fixed_gap = 0, 0
     for result in results["LightGingerTheTextResult"]:
         if(result["Suggestions"]):
-            from_index = result["From"] + color_gap
-            to_index = result["To"] + 1 + color_gap
+            from_index = result["From"] + gap
+            to_index = result["To"] + 1 + gap
             suggest = result["Suggestions"][0]["Text"]
+            fixed_text = fixed_text[:from_index-fixed_gap] + suggest + fixed_text[to_index-fixed_gap:]
 
-            # Colorize text
-            colored_incorrect = ColoredText.colorize(original_text[from_index:to_index], 'red')[0]
-            colored_suggest, gap = ColoredText.colorize(suggest, 'green')
-
-            original_text = original_text[:from_index] + colored_incorrect + original_text[to_index:]
-            fixed_text = fixed_text[:from_index-fixed_gap] + colored_suggest + fixed_text[to_index-fixed_gap:]
-
-            color_gap += gap
-            fixed_gap += to_index-from_index-len(suggest)
 
     #print("from: " + original_text)
     #print("to:   " + fixed_text)
+    
     return fixed_text
 
 
